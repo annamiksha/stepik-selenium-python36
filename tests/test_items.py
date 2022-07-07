83658dfbc240cf52621a25b15249bbe48650c781
Miksha Anna Vladimirovna <amiksha@ozon.ru>
2022-07-07 12:49:50 +0500

    add sleep and assert

--- tests/test_items.py
+++ tests/test_items.py
@@ -1,3 +1,5 @@
+from datetime import time
+
 from selenium.webdriver.common.by import By
 
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
@@ -5,5 +7,6 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_find_buy_button(browser):
     browser.get(link)
-    browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
-
+    button = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
+    time.sleep(30)
+    assert button.text is not None, 'Текст кнопки не обнаружен'
